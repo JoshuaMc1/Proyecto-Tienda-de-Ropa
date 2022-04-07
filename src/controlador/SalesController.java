e57ed05330db0ddc0d0c383d12271ca6313e012b
AvailableNameWill <carloswill06@gmail.com>
2022-04-06 23:52:57 -0600

    Metodos para validar texto
    Alli van los metodos para validar las cajas de texto, uno que le da formato a las cajas de texto DNI y telefono y metodos para validar que en una caja de texto solo se puedan ingresar numeros o solo texto

--- src/controlador/SalesController.java
+++ src/controlador/SalesController.java
@@ -1,24 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controlador;
 
+import Conexion.ConexionMySQL;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import modelo.funciones.funciones;
 
-/**
- * FXML Controller class
- *
- * @author Joshua Mc
- */
 public class SalesController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    private funciones fun = new funciones();
+    ConexionMySQL con = new ConexionMySQL(); //Variable que referencia a la clase que realiza la conexion a la bd
+    private String bd = "tienda_de_ropa_2"; //nombre de la bd
+    
+    @FXML
+    private Button btnAgProd;
+    @FXML 
+    private TextField txtIDProd;
+    @FXML
+    private AnchorPane pnlPrinc;
+    
+    //metodo para limpiar las cajas de texto en un panel
+    private void cleanTxt(AnchorPane ap, String txtName){ // El Pane es el panel en que se encuentran las cajas de texto
+        try{
+            for(Node n1 : ap.getChildren()){ //se usa un ciclo foreach y se recorren todos los objetos hijos del panel
+                //Node de javafx = Component de java.awt
+                if (n1 instanceof TextField) { //si el nodo es un textfield se limpia
+                    //if(!n1.getId().equals(txtName)) //El unico txtfld que no se limpiara es el que muestra el id del usuario
+                        ((TextField) n1).setText("");
+                        System.out.println(n1.getId().toString());
+               }
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            /*fun.msg("Ha ocurrido un error: " + e + 
+                "\n Por Favor contacte a soporte");*/
+        }
+    }    
+    
+    @FXML
+    private void agrProd(ActionEvent evt){
+        System.out.println(txtIDProd.getParent().getId());
+    }
+    
+    @FXML
+    private void saveFact(ActionEvent evt){
+        
+    }
+    
+    @FXML
+    private void removeProd(ActionEvent evt){
+        
+    }
+    
+    @FXML
+    private void cleanTxt(ActionEvent evt){
+        cleanTxt(pnlPrinc, "txtIDUsr");
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
--- src/modelo/funciones/funciones.java
+++ src/modelo/funciones/funciones.java
@@ -1,8 +1,16 @@
 
 package modelo.funciones;
 
+import java.awt.HeadlessException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.function.UnaryOperator;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.layout.Pane;
 import javax.swing.JOptionPane;
 import modelo.ConexionMySQL;
 
@@ -13,6 +21,76 @@ import modelo.ConexionMySQL;
 public class funciones {
     //Objetos
     ConexionMySQL con = new ConexionMySQL();
+    
+    public void msg(String msg){
+        JOptionPane.showMessageDialog(null, msg);
+    }
+    
+    //metodo para verificar que todas las cajas de texto en un panel esten vaciasm
+    public boolean Vacio(Pane panel,String tfName){// panel en que se encuentran las cajas de texto y opcional una caja de texto que no se quiera
+        boolean vacio = false;                         // verificar si esta vacia o no, solo se pone el id de la caja de texto
+        for(Node jt : panel.getChildren()){ //foreach, recorre todos los componentes en el panel
+            if(jt instanceof TextField){ //if un componente es un textfield
+                if(((TextField) jt).getText().isEmpty() && !jt.getId().equals(tfName)){ //if la caja de texto esta vacia o su nombre es similar al nombre de la caja de texto que no se quiere verificar
+                    vacio=true;
+                    break; 
+                }
+            }
+        }
+        return vacio;
+    }
+    
+    //metodo para cambiar de estado los datos en la base de datos
+   /* public void eliminar(String sentencia){
+        try {
+            con.ConectarBasedeDatos();
+            con.sentencia.execute(sentencia);
+            JOptionPane.showMessageDialog(null, "REGISTRO ELIMINADO CORRECTAMENTE", "ATENCION!", 1);
+            con.DesconectarBasedeDatos();
+        } catch (HeadlessException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "NO SE PUDO ELIMINAR EL REGISTRO\nERROR: "+e.getMessage(), "ATENCION!", 0);
+        }
+    }
+    
+    //metodo para modificar los datos en la base de datos
+    public void modificar(String sentencia){
+        try{
+            con.ConectarBasedeDatos();
+            con.sentencia.execute(sentencia);
+            //JOptionPane.showMessageDialog(null, "REGISTRO ACTUALIZADO CORRECTAMENTE", "ATENCION!", 1);
+            con.DesconectarBasedeDatos();
+        }catch (SQLException ex){
+            JOptionPane.showMessageDialog(null, "NO SE PUDO MODIFICAR EL REGISTRO\nERROR: "+ex.getMessage(), "ATENCION!", 0);
+        }
+    }
+    
+    //metodo para buscar en la base de datos
+    public ResultSet buscar(String sql, Conexion.ConexionMySQL con){
+        try{
+            con = new Conexion.ConexionMySQL();
+            con.ConectarBasedeDatos("supermercado");
+            PreparedStatement ps = con.getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            return rs;
+        }catch(Exception e){
+            return null;
+        }
+    }
+    
+    //metodo para guardar el la base de datos
+    public void guardar(String sentencia){
+        try{
+            con.ConectarBasedeDatos();
+            System.out.println("coencta");
+            PreparedStatement ps = con.getConnection().prepareStatement(sentencia);
+            ps.execute();
+            con.DesconectarBasedeDatos();
+            //JOptionPane.showMessageDialog(null, "REGISTRO GUARDADO CORRECTAMENTE", "ATENCION!", 1);
+        }catch(SQLException ex){
+            JOptionPane.showMessageDialog(null, "NO SE PUDO GUARDAR EL REGISTRO\nError: "+ex.getMessage(), "ATENCION!", 0);
+            ex.printStackTrace();
+        }
+    }*/
     
     public int validarInicio(String usuario, String clave){
         int dato = 0;
@@ -54,5 +132,102 @@ public class funciones {
             JOptionPane.showMessageDialog(null,e.getMessage());
         }
         return msg;
+    }
+    
+    public void formatTD(TextField tf, int bt){ //metodo para validar la caja de texto telefono y DNI
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+        if (change.isContentChange()) { //si hay cambios en la caja de texto
+            handleBackspaceOverSpecialCharacter(change);
+            if (change.getText().matches("[0-9]*")) { //si el texto capturado por el textformatter es igual a un valor numerico
+                int originalNewTextLength = change.getControlNewText().length();
+                change.setText(formatNumber(change.getControlNewText(), bt)); /*se le aplica el formato al texto y se guarda en la variable
+                change el nuevo texto en la caja*/
+                change.setRange(0, change.getControlText().length());
+                int caretOffset = change.getControlNewText().length() - originalNewTextLength;
+                change.setCaretPosition(change.getCaretPosition() + caretOffset);
+                change.setAnchor(change.getAnchor() + caretOffset);
+                /*esto sirve para colocar el puntero en el ultimo digito y no permitir que se vayan arrastrando datos en la caja de
+                texto*/
+                return change;
+            } else {
+                return null;
+            }
+        }
+        return change;
+        };
+        TextFormatter<String> formatter = new TextFormatter<>(filter);
+        tf.setTextFormatter(formatter);
+    }
+    
+    public void validaTexto(TextField tf, int longitud){ //Metodo para validar que solo se ingresen letras y espacios
+        UnaryOperator<TextFormatter.Change> filter = change ->{ 
+            if (change.getControlNewText().matches("[a-zA-Z ]*") && change.getControlNewText().length() <= longitud){ /*si el texto que se encuentra en la caja de texto es una letra o 
+                un espacio devuelve ese texto*/
+                return change; 
+            }
+            return null; //si no no devuelve nada o sea no permite que se ingrese el valor tecleado
+        };
+        TextFormatter<String> formatter = new TextFormatter<>(filter);
+          tf.setTextFormatter(formatter);
+    }
+    
+   public void validaNumeros(TextField tf,  int longitud){
+       UnaryOperator<TextFormatter.Change> filter = change ->{ 
+            if (change.getControlNewText().matches("[0-9]*") && change.getControlNewText().length() <= longitud){ /*si el texto que se encuentra en la caja de texto es un numero*/
+                return change; 
+            }
+            return null; //si no no devuelve nada o sea no permite que se ingrese el valor tecleado
+        };
+        TextFormatter<String> formatter = new TextFormatter<>(filter);
+        tf.setTextFormatter(formatter);
+   }
+    
+   //Metodo para validar la longitud en una caja de texto, asi se evita que el usuario ingrese un valor mayor al permitido en la bd
+   public void manejaLongitudTxt(TextField tf, int longitud){ 
+       UnaryOperator<TextFormatter.Change> filter = change ->{ 
+            if (change.getControlNewText().length() <= longitud){ /*si la longitud del texto es menor a la longitud deseada por el usuario*/
+                return change; //Permite escribir
+            }
+            return null; //si no no devuelve nada o sea no permite que se ingrese el valor tecleado
+        };
+        TextFormatter<String> formatter = new TextFormatter<>(filter);
+        tf.setTextFormatter(formatter);
+   }
+   
+    private void handleBackspaceOverSpecialCharacter(TextFormatter.Change change) { /*Este metodo permite borrar todos los valores de
+        una caja de texto que use el metodo formatTD(), sin este metodo quedaria un numero y un guion que no se iban a poder eliminar*/
+        if (change.isDeleted() && (change.getSelection().getLength() == 0)) {
+            if (!Character.isDigit(change.getControlText().charAt(change.getRangeStart()))) {
+                if (change.getRangeStart() > 0) {
+                    change.setRange(change.getRangeStart() - 1, change.getRangeEnd() - 1);
+                }
+            }
+        }
+    }
+
+    private String formatNumber(String numbers, int tb) { //metodo para darle formato a la caja de texto de telefono y DNI
+        String nFmt="", fNum="";
+        numbers = numbers.replaceAll("[^\\d]", ""); //Reemplaza cualquier valor que no sea un numero por un espacio en blanco
+        
+        if(tb == 1) fNum = numbers.substring(0, Math.min(8, numbers.length()));
+        else fNum = numbers.substring(0, Math.min(13, numbers.length()));;
+        //Esto controla la cantidad de numeros que se pueden ingresar
+        nFmt = fNum; 
+        if (nFmt.length() == 0) return "";
+        if(tb == 1){
+        if(nFmt.length() < 5) return nFmt + "-";
+        nFmt = nFmt.replaceFirst("(\\d{4})(\\d+)", "$1-$2");
+        //si no hay coincidencias con los if statement
+        //(\\d{4}) = valores que sean digitos y esten agrupados en un grupo de 4
+        //(\\d+) = valores que sean digitos y se signa tecleando
+        //$1 = el primer grupo definido dentro de los parentesis
+        //- = el separador
+        //$2 = el segundo grupo definido dentro de parentesis
+        } else{
+            if(nFmt.length() < 5) return nFmt + "-";
+            if(nFmt.length() < 9) return nFmt.replaceFirst("(\\d{4})(\\d+)", "$1-$2");
+            nFmt = nFmt.replaceFirst("(\\d{4})(\\d{4})(\\d+)", "$1-$2-$3");
+        }
+        return nFmt;
     }
 }
