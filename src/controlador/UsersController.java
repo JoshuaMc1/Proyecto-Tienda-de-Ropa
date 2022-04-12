9567b08e949054ea5870cc3aa82ff624576a80a8
YosaryBb <98617502+YosaryBb@users.noreply.github.com>
2022-04-12 00:52:06 -0600

    excel
    un excel

--- src/controlador/UsersController.java
+++ src/controlador/UsersController.java
@@ -5,10 +5,25 @@
  */
 package controlador;
 
+import controlador.clases.Fact_Model;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javax.swing.table.DefaultTableModel;
+import modelo.ConexionMySQL;
 
 /**
  * FXML Controller class
@@ -16,8 +31,46 @@ import javafx.scene.input.MouseEvent;
  * @author Joshua Mc
  */
 public class UsersController implements Initializable {
+      
+ 
+    
+    
+    String cliente, vendedor, idPropiedad;
+    @FXML
+    private TextField txtDNI;
+    @FXML
+    private TextField txtnombre;
+    @FXML
+    private TextField txtapellido;
+    @FXML
+    private TextField txttelefono;
+    @FXML
+    private TextArea txtdireccion;
+    @FXML
+    private TextField txtedad;
+    @FXML
+    private ComboBox<?> cmbgenero;
+    @FXML
+    private Button btnguardar;
+    @FXML
+    private Button btneditar;
+    @FXML
+    private TextField txtusuario;
+    @FXML
+    private PasswordField txtcontra;
+    @FXML
+    private CheckBox chec1;
+    @FXML
+    private CheckBox chec2;
+    @FXML
+    private CheckBox chec4;
+    @FXML
+    private CheckBox chec3;
+    @FXML
+    private TableView<?> tbusuario;
 
-
+    
+  
     /**
      * Initializes the controller class.
      */
